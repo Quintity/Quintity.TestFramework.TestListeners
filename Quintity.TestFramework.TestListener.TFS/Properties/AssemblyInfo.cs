953534144535ff69e26168a3f77aeb5c8ddf7b31
James Mothershead <jmothershead@quintity.com>
2018-05-30 12:34:55 -0700

    Cleaned up references and versioning info.

--- Quintity.TestFramework.TestListener.TFS/Properties/AssemblyInfo.cs
+++ Quintity.TestFramework.TestListener.TFS/Properties/AssemblyInfo.cs
@@ -15,3 +15,17 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("db45a100-515d-48b1-9060-3325fe502e3f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
--- Quintity.TestFramework.TestListener.VersionOne/Properties/AssemblyInfo.cs
+++ Quintity.TestFramework.TestListener.VersionOne/Properties/AssemblyInfo.cs
@@ -15,3 +15,16 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bde58dbc-0cf5-4094-b9f6-5da97a726b26")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
